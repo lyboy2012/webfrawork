a0da0b5ab9f9f96aada94a05ebeba6825e01ce03
liying <ly.boy2012@gmail.com>
2016-02-24 10:13:09 +0800

    通过webpack loader-babel 预编译es6

---
+++ velocity/js/__build/index.js
@@ -0,0 +1,19 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function($) {'use strict';
+
+	var _config = __webpack_require__(2);
+
+	var _config2 = _interopRequireDefault(_config);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	//alert(config.NUM);
+		console.log($('h1').text() + '-------------- index' + _config2.default.NUM);
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }
+]);
+//# sourceMappingURL=index.js.map
---
+++ velocity/js/source/entry/index.es6
@@ -0,0 +1,4 @@
+'use strict';
+import config from '../common/config'
+//alert(config.NUM);
+console.log($('h1').text()+'-------------- index'+config.NUM);
