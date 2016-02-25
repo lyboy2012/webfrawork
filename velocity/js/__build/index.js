3b0a9d22aed9cfd9ae832c51f2c42a77708782cd
liying <ly.boy2012@gmail.com>
2016-02-25 17:38:14 +0800

    一些常规测试

--- velocity/js/__build/index.js
+++ velocity/js/__build/index.js
@@ -11,7 +11,15 @@ webpackJsonp([1],[
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	//alert(config.NUM);
-		console.log($('h1').text() + '-------------- index' + _config2.default.NUM);
+	console.log($('h1').text() + '-------------- index' + _config2.default.NUM);
+
+	var names = { "a": "a", "b": "b" };
+	function a() {
+		var arrNames = Array.prototype.slice(arguments, 0);
+
+		$('h1').text(Object.prototype.toString.call(arrNames) === '[object Array]');
+	}
+		a(1, 2, 3);
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
 
 /***/ }
--- velocity/js/source/entry/index.es6
+++ velocity/js/source/entry/index.es6
@@ -2,3 +2,11 @@
 import config from '../common/config'
 //alert(config.NUM);
 console.log($('h1').text()+'-------------- index'+config.NUM);
+
+const names = {"a":"a","b":"b"};
+function a(){
+	let arrNames = Array.prototype.slice(arguments,0);
+
+	$('h1').text(Object.prototype.toString.call(arrNames)==='[object Array]')
+}
+a(1,2,3)
